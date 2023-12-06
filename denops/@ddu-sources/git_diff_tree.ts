affb3a779002ccebbc583583922a93bf52280753
GitHub <noreply@github.com>
2023-12-06 09:03:22 +0000

    chore(deps): Update deno dependencies

--- denops/@ddu-sources/git_diff_tree.ts
+++ denops/@ddu-sources/git_diff_tree.ts
@@ -1,10 +1,10 @@
-import type { GatherArguments } from "https://deno.land/x/ddu_vim@v3.6.0/base/source.ts";
-import { Denops, fn } from "https://deno.land/x/ddu_vim@v3.6.0/deps.ts";
-import { treePath2Filename } from "https://deno.land/x/ddu_vim@v3.6.0/utils.ts";
+import type { GatherArguments } from "https://deno.land/x/ddu_vim@v3.8.1/base/source.ts";
+import { Denops, fn } from "https://deno.land/x/ddu_vim@v3.8.1/deps.ts";
+import { treePath2Filename } from "https://deno.land/x/ddu_vim@v3.8.1/utils.ts";
 import type { ActionData as FileActionData } from "https://deno.land/x/ddu_kind_file@v0.7.1/file.ts";
 
-import { BaseSource, Item } from "https://deno.land/x/ddu_vim@v3.6.0/types.ts";
-import { TextLineStream } from "https://deno.land/std@0.206.0/streams/text_line_stream.ts";
+import { BaseSource, Item } from "https://deno.land/x/ddu_vim@v3.8.1/types.ts";
+import { TextLineStream } from "https://deno.land/std@0.208.0/streams/text_line_stream.ts";
 import { ChunkedStream } from "https://deno.land/x/chunked_stream@0.1.2/mod.ts";
 
 type ActionData = FileActionData;
