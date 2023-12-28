56684b7cb87d51cae642d4256190fb37a14424be
kyoh86 <me@kyoh86.dev>
2023-12-29 07:14:27 +0900

    Update dependencies

--- denops/@ddu-sources/git_diff_tree.ts
+++ denops/@ddu-sources/git_diff_tree.ts
@@ -1,9 +1,9 @@
-import type { GatherArguments } from "https://deno.land/x/ddu_vim@v3.8.1/base/source.ts";
-import { Denops, fn } from "https://deno.land/x/ddu_vim@v3.8.1/deps.ts";
-import { treePath2Filename } from "https://deno.land/x/ddu_vim@v3.8.1/utils.ts";
+import type { GatherArguments } from "https://deno.land/x/ddu_vim@v3.9.0/base/source.ts";
+import { Denops, fn } from "https://deno.land/x/ddu_vim@v3.9.0/deps.ts";
+import { treePath2Filename } from "https://deno.land/x/ddu_vim@v3.9.0/utils.ts";
 import type { ActionData as FileActionData } from "https://deno.land/x/ddu_kind_file@v0.7.1/file.ts";
 
-import { BaseSource, Item } from "https://deno.land/x/ddu_vim@v3.8.1/types.ts";
+import { BaseSource, Item } from "https://deno.land/x/ddu_vim@v3.9.0/types.ts";
 import { TextLineStream } from "https://deno.land/std@0.210.0/streams/text_line_stream.ts";
 import { ChunkedStream } from "https://deno.land/x/chunked_stream@0.1.2/mod.ts";
 
